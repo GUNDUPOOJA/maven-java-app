d50845452d2c2fefd388e77454a83de0a56233a4
gundu pooja <S538295@nwmissouri.edu>
2021-02-19 14:33:53 -0600

    initial commit

---
+++ src/main/java/edu/nwmissouri/bigdata/gundu/pooja/App.java
@@ -0,0 +1,13 @@
+package edu.nwmissouri.bigdata.gundu.pooja;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Bearcats!-from Pooja" );
+    }
+}
